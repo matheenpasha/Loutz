b7bee272001a94cd337669da53a0b4fc45a02c9f
matheenpasha <Mat@Matheens-MacBook-Air.local>
2015-09-21 22:46:53 +0530

    cookies

--- NodeJS/Streams_And_Servers/Server.js
+++ NodeJS/Streams_And_Servers/Server.js
@@ -31,7 +31,7 @@ server.listen(8080);
 
 
 //-------- tunelling using a proxy
-
+/*
 
 var http = require('http');
 var net = require('net');
@@ -68,3 +68,33 @@ request.on('connect', function(res, socket, head){
     proxy.close();
   })
 });
+
+*/
+
+
+//-----------------------setting a session cookie
+
+var http = require('http');
+var url = require('url');
+var server = http.createServer(function(req, res) {
+  var cookies = req.headers.cookie;
+  if(!cookies) {
+    var cookieName = "AppSession", cookieValue = 123456, expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + 1);
+    var cookieText = cookieName + '=' + cookieValue + ';expires' + expiryDate.toUTCString() + ';';
+    res.setHeader('Set-Cookie', cookieText);
+    res.writeHead(302, {
+      'location': '/'
+    });
+    return res.end();
+  }
+
+  cookies.split(';').forEach(function(cookie) {
+    var m = cookie.match(/(.*?)=(.*)$/);
+    console.log(m);
+    cookies[m[1].trim()] = (m[2] || '').trim();
+  });
+  res.writeHead(200);
+  res.end('cookie set:' + cookies.toString());
+
+}).listen(8080);
